980c72ad0e6f2499113876d7507994adb61f2d02
smena8 <puttinonthefritz@protonmail.com>
2022-09-21 16:16:32 -0700

    django heroku settings import

--- fruitcrush/settings.py
+++ fruitcrush/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -125,3 +127,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG is False:
+    django_heroku.settings(locals())
