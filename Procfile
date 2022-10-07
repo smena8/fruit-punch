9d0d508e2c26186159ecea73510a7829fdaee980
smena8 <puttinonthefritz@protonmail.com>
2022-10-07 13:28:06 -0700

    cleaning files for move to github.io

--- Procfile
+++
@@ -1,1 +0,0 @@
-web gunicorn fruitcrush.wsgi:application --log-file -
--- game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'FruitCrush'
-urlpatterns = [
-    path('', views.index, name='Game')
-]
